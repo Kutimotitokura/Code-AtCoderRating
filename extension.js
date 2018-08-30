03708ca0f0cd10efba43bf4c567ce0a5252fd84d
Kutimotitokura <kutimoti.tokura@gmail.com>
2018-08-30 11:51:35 +0900

    [update] show rate

--- extension.js
+++ extension.js
@@ -1,6 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const client = require('cheerio-httpcli');
+
+var rating = "0";
+
+var statusBarRating = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+
+var getAtCoderRating = function () {
+    var username = 'kutimotitokura'
+    client.fetch(`https://beta.atcoder.jp/users/${username}`, {}, function (err, $, res) {
+        console.log(res.headers);
+
+        console.log($('title').text());
+        console.log($('#main-container > div > div.col-sm-8 > table > tr:nth-child(2) > td').html());
+        rating = $('#main-container > div > div.col-sm-8 > table > tr:nth-child(2) > td > span').html();
+        statusBarRating.hide();
+        statusBarRating.text = `${username}:${rating}`;
+        statusBarRating.color = ['gray','brown','green','cyan','#494cff','yellow','orange','red'][Math.min(Math.floor(parseInt(rating) / 400),7)];
+        statusBarRating.show();
+    });
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,6 +39,9 @@ function activate(context) {
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
     });
+
+    getAtCoderRating();
+
 
     context.subscriptions.push(disposable);
 }
